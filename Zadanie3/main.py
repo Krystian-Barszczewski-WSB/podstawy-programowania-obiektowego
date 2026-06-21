9c5d627532fbe2da7eb316b8d72b1fe43526fa7e
Felt <pldrzewiec@gmail.com>
2026-06-21 22:33:15 +0200

    Dodanie klasy Order (zamówienia) i ElectronicProd

--- Zadanie3/main.py
+++ Zadanie3/main.py
@@ -30,6 +30,14 @@ class FoodProd(Product):
     def get_info(self):
         return f"Food: {self._name}, quantity: {self._quantity}, expiration date: {self.expiry_date}"
 
+class ElectronicProd(Product):
+    def __init__(self, product_id, name, quantity, warranty_months):
+        super().__init__(product_id, name, quantity)
+        self.warranty_months = warranty_months
+
+    def get_info(self):
+        return f"Electronics: {self._name}, quantity: {self._quantity}, warranty: {self.warranty_months} months"
+
 class Warehouse:
     def __init__(self, warehouse_id, name):
         self.id = warehouse_id
@@ -43,12 +51,34 @@ class Warehouse:
         for product in self.products:
             print(product.get_info())
 
+class Order:
+    def __init__(self, order_id, product, quantity):
+        self.id = order_id
+        self.product = product
+        self.quantity = quantity
+
+    def process(self):
+        self.product.remove_stock(self.quantity)
+
 def main():
-    mleko = FoodProd(1, "melko", 1, "21.07.2026")
+    mleko = FoodProd(1, "mleko", 1, "21.07.2026")
     mleko.add_stock(5)
 
-    magazyn = Warehouse(0, "Magazyn jedzenia")
+    telefon = ElectronicProd(23, "telefon", 24, "12")
+
+    jablko = FoodProd(2, "jabłko", 1, "25.08.2026")
+
+    magazyn = Warehouse(0, "Magazyn 1")
     magazyn.add_product(mleko)
+    magazyn.add_product(telefon)
+    magazyn.add_product(jablko)
+
+    magazyn.show_products()
+
+    print("-----------------------------------------------------------")
+
+    zamow_tel = Order(5, telefon, 10)
+    zamow_tel.process()
 
     magazyn.show_products()
 
