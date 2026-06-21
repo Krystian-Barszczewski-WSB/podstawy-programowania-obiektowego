9ea8e108b8a1259fb84b04ae892944371c64f154
Felt <pldrzewiec@gmail.com>
2026-06-21 22:10:34 +0200

    Dodanie abstrakcji i klasy Warehouse

--- Zadanie3/main.py
+++ Zadanie3/main.py
@@ -1,4 +1,6 @@
-class Product:
+from abc import ABC, abstractmethod
+
+class Product(ABC):
     def __init__(self, product_id, name, quantity):
         self._id = product_id
         self._name = name
@@ -16,11 +18,39 @@ class Product:
         if 0 < amount <= self._quantity:
             self._quantity -= amount
 
+    @abstractmethod
+    def get_info(self):
+        pass
+
+class FoodProd(Product):
+    def __init__(self, product_id, name, quantity, expiry_date):
+        super().__init__(product_id, name, quantity)
+        self.expiry_date = expiry_date
+
+    def get_info(self):
+        return f"Food: {self._name}, quantity: {self._quantity}, expiration date: {self.expiry_date}"
+
+class Warehouse:
+    def __init__(self, warehouse_id, name):
+        self.id = warehouse_id
+        self.name = name
+        self.products = []
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def show_products(self):
+        for product in self.products:
+            print(product.get_info())
+
 def main():
-    mleko = Product(1, "melko", 1)
+    mleko = FoodProd(1, "melko", 1, "21.07.2026")
     mleko.add_stock(5)
 
-    print(mleko._name)
+    magazyn = Warehouse(0, "Magazyn jedzenia")
+    magazyn.add_product(mleko)
+
+    magazyn.show_products()
 
 if __name__ == '__main__':
     main()
